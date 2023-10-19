bbfd814728cae45217acfb313655608b1f7228fa
migueltomasamat <migueltomas.informatica@iespacomolla.es>
2023-10-19 16:52:28 +0200

    clase 19/10/2023

--- src/videoclub/main.php
+++ src/videoclub/main.php
@@ -2,9 +2,13 @@
 
 include_once "./Soporte.php";
 include_once "./CintaVideo.php";
+include_once "./Juego.php";
+include_once "./DVD.php";
 
 use Videoclub\Soporte;
 use Videoclub\CintaVideo;
+use Videoclub\Juego;
+use Videoclub\DVD;
 
 $soporte1= new Soporte("Batman",1);
 
@@ -14,3 +18,20 @@ echo "<br>";
 $soporte2= new CintaVideo("El resplandor",2,104,6.5);
 
 $soporte2->muestraResumen();
+echo "<br>";
+
+
+$soporte3 = new Juego("Gran Turistmo",3,4);
+
+$soporte3->muestraResumen();
+echo "<br>";
+$soporte3->muestraJugadoresPosibles();
+echo "<br>";
+
+
+$soporte4 = new DVD("Animatrix",4,"es-en","Imax",10);
+
+$soporte4->muestraResumen();
+echo "<br>";
+
+echo $soporte4;
